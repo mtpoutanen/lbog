4fef0b26001f262d7fed8c05f83bf360303029e9
mikko <m.poutanen.12@ucl.ac.uk>
2013-08-12 18:11:09 +0100

    'test'

--- lbog/context_processors.py
+++ lbog/context_processors.py
@@ -1,4 +1,7 @@
 from django.conf import settings # import the settings file
 
 def fb_app_id(request):
-    return {'FB_APP_ID': settings.FACEBOOK_APP_ID}
+    return {
+    	'FB_APP_ID': settings.FACEBOOK_APP_ID,
+    	'SITE_ROOT': settings.ALLOWED_HOSTS[0],
+    	}
